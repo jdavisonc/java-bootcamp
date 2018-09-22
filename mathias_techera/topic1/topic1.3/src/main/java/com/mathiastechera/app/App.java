a3e8388ad5552b030eb623582e8e2b123b7071a4
Mathias Techera <techoogler@gmail.com>
2018-09-22 15:46:17 -0300

    Topic 1

---
+++ mathias_techera/topic1/topic1.3/src/main/java/com/mathiastechera/app/App.java
@@ -0,0 +1,15 @@
+package com.mathiastechera.app;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Math example using Maven." );
+    }
+
+
+    public int add(int a, int b){
+    	return a + b;
+    }
+
+}
