5ff848387508eaa8ca77a5c06a9571cf6b001a41
Santiago Kent <santiagokent@gmail.com>
2018-09-27 16:10:06 -0300

    Cleaned tree for PR

--- Topic 0/src/abstractFactory/MySqlTwo.java
+++
@@ -1,9 +0,0 @@
-package abstractFactory;
-
-public class MySqlTwo implements Database {
-	@Override
-	public String connect() {
-		return "Connected to MySql Two";
-	}
-
-}
--- Topic 0/src/abstractFactory/OracleTwo.java
+++
@@ -1,9 +0,0 @@
-package abstractFactory;
-
-public class OracleTwo implements Database{
-	@Override
-	public String connect() {
-		return "Connected to OracleTwo";
-	}
-
-}
