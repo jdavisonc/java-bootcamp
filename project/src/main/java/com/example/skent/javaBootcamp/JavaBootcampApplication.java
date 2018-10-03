85a8e307bdaed59bb87d09d23ebdfd713366e561
Santiago Kent <santiagokent@gmail.com>
2018-10-03 15:31:22 -0300

    removed untracked files

---
+++ project/src/main/java/com/example/skent/javaBootcamp/JavaBootcampApplication.java
@@ -0,0 +1,12 @@
+package com.example.skent.javaBootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBootcampApplication.class, args);
+	}
+}
